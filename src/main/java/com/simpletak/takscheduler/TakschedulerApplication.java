04b19875cc1a72de42629d2e3055d4964aa3a8ab
LordNani <rigansu@gmail.com>
2021-09-19 16:26:43 +0300

    Init commit

---
+++ src/main/java/com/simpletak/takscheduler/TakschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.simpletak.takscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpletak/takscheduler/TakschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpletak.takscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
