eade3f1c7eb23fb073a2eb9e9784fd8f800479b3
kirill26031 <kirill26031@gmail.com>
2021-09-29 20:52:25 +0300

    Added annotations to DTOs

--- backend/src/main/java/com/simpletak/takscheduler/dto/subscription/SubscriptionDTO.java
+++ backend/src/main/java/com/simpletak/takscheduler/dto/subscription/SubscriptionDTO.java
@@ -1,4 +1,11 @@
 package com.simpletak.takscheduler.dto.subscription;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SubscriptionDTO {
 }
