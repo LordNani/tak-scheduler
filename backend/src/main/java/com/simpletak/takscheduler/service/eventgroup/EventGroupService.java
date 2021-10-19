deed85bd187605ea41fa1e1f317307c63505a40b
HellaxA <abereznikov64@gmail.com>
2021-10-19 21:28:03 +0300

    Implemented Log4j2 config with Markers and 2 LOG files

--- backend/src/main/java/com/simpletak/takscheduler/service/eventgroup/EventGroupService.java
+++ backend/src/main/java/com/simpletak/takscheduler/service/eventgroup/EventGroupService.java
@@ -2,6 +2,10 @@ package com.simpletak.takscheduler.service.eventgroup;
 
 import com.simpletak.takscheduler.model.eventGroup.EventGroupEntity;
 import com.simpletak.takscheduler.repository.eventGroup.EventGroupRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,10 +14,13 @@ import java.util.UUID;
 
 @Service
 public class EventGroupService {
-
-    private EventGroupRepository eventGroupRepository;
-
+private EventGroupRepository eventGroupRepository;
+    private static final Marker ADMIN_USER = MarkerManager.getMarker("ADMIN");
     public EventGroupEntity findEventGroupById(UUID id) {
+        Logger logger = LogManager.getLogger(getClass());
+        logger.info("findEventGroupById: {}", id);
+        logger.error(ADMIN_USER, "admin: findEventGroupById: {}", id);
+        logger.error("not admin: findEventGroupById: {}", id);
         return eventGroupRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
