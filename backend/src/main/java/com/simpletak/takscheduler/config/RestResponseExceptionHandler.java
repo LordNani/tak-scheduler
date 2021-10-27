f2fcfb7fd75e8a9df0224a678cb5e8cac6e1162f
LordNani <rigansu@gmail.com>
2021-10-27 19:33:58 +0300

    Added custom error and exception controller

--- backend/src/main/java/com/simpletak/takscheduler/config/RestResponseExceptionHandler.java
+++ backend/src/main/java/com/simpletak/takscheduler/config/RestResponseExceptionHandler.java
@@ -1,11 +1,19 @@
 package com.simpletak.takscheduler.config;
 
 import com.simpletak.takscheduler.exception.WebException;
+import com.simpletak.takscheduler.exception.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
@@ -23,6 +31,27 @@ public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Response.error("Internal server error"));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Response<Void>> handleWebException (UserNotFoundException e) {
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(Response.error("Testing user not correct"));
+    }
+
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+        return new ResponseEntity<Object>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
